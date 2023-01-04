5f18fa78c6a6f8f8d676edfa5631de9db5435cc2
Ranaelle <modoranaelle@gmail.com>
2023-01-04 10:11:04 -0600

    added code to add booking in the Car Dao added code to generate a booking ref added code to generate booking date

--- src/com/ranaelle/booking/BookingService.java
+++ src/com/ranaelle/booking/BookingService.java
@@ -1,5 +1,7 @@
 package com.ranaelle.booking;
 
+import java.util.UUID;
+
 public class BookingService {
 
     private BookingDAO bookingDAO = new BookingDAO();
@@ -9,11 +11,24 @@ public class BookingService {
            throw new IllegalArgumentException(
                    "Booking cannot be null"
            );
+       }else{
+           bookingDAO.saveBookings(booking);
        }
 
 
         return 1;
     }
+
+    public void generateBookingRef(){
+        UUID uuid = UUID.randomUUID();
+        String bookingRef = uuid.toString();
+    }
+
+    public void generateBookingDate(){
+
+    }
+
+
 
     public Booking[] getAllBookings(){
 
